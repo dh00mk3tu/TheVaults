1cd5a5dcc4cc4e06f936bd02cf1e7904667b4a25
dh00mk3tu <cyberhybird@hotmail.com>
2021-03-18 12:24:50 +0530

    This commit is made by Auto-Git. Commit-87, Thursday 18 March 2021 10:53:54 AM IST

--- Resources/Dependencies/JS/vault-js.js
+++ Resources/Dependencies/JS/vault-js.js
@@ -1,11 +1,1 @@
-var load = function () {
-    console.log("Funtion Called");
-}
-
-document.getElementById('cmd-inpt').onkeypress = function(e){
-    if (!e) e = window.event;
-    if (e.keyCode == '13'){
-      alert(this.value);
-      return false;
-    }
-  }
+console.log("Script Loaded");
